b175e1773888f4985778a1e30d0d0cfe588a23a3
tpatzelt <tpatzelt@uos.de>
2022-02-03 18:47:35 +0100

    Set data filepaths to external of repo ( windowed data).

--- drowsiness_detection/config.py
+++ drowsiness_detection/config.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 
-DATA_PATH = Path(__file__).parent.parent.joinpath("data/")
-PREPROCESSED_DATA_PATH = DATA_PATH.joinpath("preprocessed")
+# DATA_PATH = Path(__file__).parent.parent.joinpath("data/")
+# PREPROCESSED_DATA_PATH = DATA_PATH.joinpath("preprocessed")
+
+DATA_PATH = Path("/home/tim/Windows/")
+WINDOW_DATA_PATH = DATA_PATH.joinpath("WindowData")
+WINDOW_LABEL_PATH = DATA_PATH.joinpath("WindowLabels")
+WINDOW_FEATURES_PATH = DATA_PATH.joinpath("WindowFeatures")
+DATA_FORMAT_PATH = DATA_PATH.joinpath("data_format.json")
+LABEL_FORMAT_PATH = DATA_PATH.joinpath("kss_labels_format.json")
+FEATURE_NAMES_PATH = DATA_PATH.joinpath("features_names.txt")
