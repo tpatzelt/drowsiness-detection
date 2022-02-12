d15542468a7e1ef143399e224063777c3a2dc0ab
tpatzelt <tpatzelt@uos.de>
2022-02-12 15:37:23 +0100

    Implement function to plot a set of values for each parameter in a gridsearch using the set of best performing values for the other parameters.

--- drowsiness_detection/visualize.py
+++ drowsiness_detection/visualize.py
@@ -263,3 +263,47 @@ def plot_roc_over_subjects(classifier, identifiers: np.ndarray, X: np.ndarray, y
     ax.legend(loc="lower right")
     ax.legend().set_visible(False)
     plt.show()
+
+
+def plot_search_results(grid):
+    """
+    Plots the grid values of each parameter at
+    a time against the fixed set of best values
+    of the other parameters.
+    Note: The type of parameters should be uniform
+    for plotting/labeling the x-axis. For example,
+    'max_depth = [1,2,None]' does not work.
+
+    Params:
+        grid: A trained GridSearchCV object.
+    """
+    ## Results from grid search
+    results = grid.cv_results_
+    means_test = results['mean_test_score']
+    stds_test = results['std_test_score']
+    # means_train = results['mean_train_score']
+    # stds_train = results['std_train_score']
+
+    ## Getting indexes of values per hyper-parameter
+    masks = []
+    masks_names = list(grid.best_params_.keys())
+    for p_k, p_v in grid.best_params_.items():
+        masks.append(list(results['param_' + p_k].data == p_v))
+
+    params = grid.param_grid
+
+    ## Ploting results
+    fig, ax = plt.subplots(1, len(params), sharex='none', sharey='all', figsize=(20, 5))
+    fig.suptitle(f'Score per parameter of {str(grid.estimator)}')
+    fig.text(0.04, 0.5, f'mean {grid.scoring}'.upper(), va='center', rotation='vertical')
+    for i, p in enumerate(masks_names):
+        m = np.stack(masks[:i] + masks[i + 1:])
+        best_parms_mask = m.all(axis=0)
+        best_index = np.where(best_parms_mask)[0]
+        x = np.array(params[p])
+        y_1 = np.array(means_test[best_index])
+        e_1 = np.array(stds_test[best_index])
+        ax[i].errorbar(x, y_1, e_1, linestyle='--', marker='o')
+        ax[i].set_xlabel(p.upper())
+
+    plt.show()
