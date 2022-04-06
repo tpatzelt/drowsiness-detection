53c484fa252b663f6c7bd6e722e5d95e6109c603
tpatzelt <tpatzelt@uos.de>
2022-04-06 12:31:09 +0200

    Set config paths with integers instead of strings.

--- drowsiness_detection/config.py
+++ drowsiness_detection/config.py
@@ -2,14 +2,19 @@ from pathlib import Path
 
 SOURCES_ROOT_PATH = Path(__file__).parent
 
-BASE_PATH = Path("/home/tim/Windows")
+# BASE_PATH = Path("/home/tim/Windows")
+BASE_PATH = Path("/media/tim/My Passport/drowsiness_data/Windows")
 PATHS = None
 
 
 class Paths:
     """Object holding all data paths."""
 
-    def __init__(self, frequency_string: str, seconds_string: str, label_string: str = "KSS"):
+    def __init__(self, frequency: int, seconds: int, label_string: str = "KSS"):
+        self.frequency = frequency
+        frequency_string = str(frequency) + "_Hz"
+        self.seconds = seconds
+        seconds_string = str(seconds) + "_sec"
         self.DATA = BASE_PATH.joinpath("Windows_" + frequency_string)
         self.WINDOW_DATA = self.DATA.joinpath("WindowData").joinpath(seconds_string)
         self.LABEL_DATA = self.DATA.joinpath("WindowLabels").joinpath(label_string).joinpath(
@@ -29,7 +34,7 @@ class Paths:
 def set_paths(frequency: int, seconds: int):
     "Switch function to change data sources."
     global PATHS
-    PATHS = Paths(frequency_string=str(frequency) + "_Hz", seconds_string=str(seconds) + "_sec")
+    PATHS = Paths(frequency=frequency, seconds=seconds)
 
 
 set_paths(1, 1)
