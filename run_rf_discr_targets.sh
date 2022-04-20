c8a047a6c48b278340a14fbbbeb7525cdea096e6
Tim Patzelt <patzelt.tim@gmail.com>
2022-04-20 17:02:55 +0200

    Add discretized target variable experiment.

---
+++ run_rf_discr_targets.sh
@@ -0,0 +1,5 @@
+source venv/bin/activate;
+export PYTHONPATH=.
+python /home/tim/PycharmProjects/drowsiness-detection/drowsiness_detection/run_rf_on_dicretized_targets.py with num_targets=3
+python /home/tim/PycharmProjects/drowsiness-detection/drowsiness_detection/run_rf_on_dicretized_targets.py with num_targets=5
+python /home/tim/PycharmProjects/drowsiness-detection/drowsiness_detection/run_rf_on_dicretized_targets.py with num_targets=9
